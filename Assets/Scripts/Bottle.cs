9f17a1f5e9a60ff02ced1005e230b776541198aa
Aman Yadav <yaman6461@gmail.com>
2024-09-26 10:57:02 +0530

    waterfill game 4 update

--- Assets/Scripts/Bottle.cs
+++ Assets/Scripts/Bottle.cs
@@ -1,11 +1,62 @@
 
 
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class Bottle : MonoBehaviour
+//{
+//    public Image[] liquidImages;
+//    public int maxCapacity = 4;
+//    public int currentFillLevel = 0;
+
+//    private void Awake()
+//    {
+//        if (liquidImages == null || liquidImages.Length != maxCapacity)
+//        {
+//            liquidImages = new Image[maxCapacity];
+//        }
+
+//    }
+
+
+
+//    public bool AddImage(Image image)
+//    {
+//        if (currentFillLevel < maxCapacity)
+//        {
+//            liquidImages[currentFillLevel] = image;
+//            //liquidImages[maxCapacity - currentFillLevel -1] = image;
+
+//            currentFillLevel++;
+//            return true;
+//        }
+//        return false;
+//    }
+
+//    public Image RemoveImage()
+//    {
+//        if (currentFillLevel > 0)
+//        {
+//            currentFillLevel--;//removed
+//            Image image = liquidImages[currentFillLevel];
+//           // Image image = liquidImages[maxCapacity - currentFillLevel];
+//            //liquidImages[maxCapacity - currentFillLevel] = null;
+//            liquidImages[currentFillLevel] = null; // Clear the reference
+//            return image;
+//        }
+
+//        return null;
+//    }
+
+
+//}
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Bottle : MonoBehaviour
 {
-    public Image[] liquidImages; 
+    public Image[] liquidImages;
     public int maxCapacity = 4;
     public int currentFillLevel = 0;
 
@@ -17,16 +68,12 @@ public class Bottle : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        Debug.Log(this + " current fill level: " + currentFillLevel);
-    }
-
     public bool AddImage(Image image)
     {
         if (currentFillLevel < maxCapacity)
         {
-            liquidImages[currentFillLevel] = image;
+            // Add image to the "top" of the bottle (end of the array in reverse)
+            liquidImages[maxCapacity - currentFillLevel - 1] = image;
             currentFillLevel++;
             return true;
         }
@@ -37,10 +84,20 @@ public class Bottle : MonoBehaviour
     {
         if (currentFillLevel > 0)
         {
+            // Remove image from the "top" of the bottle (end of the array in reverse)
+            Image image = liquidImages[maxCapacity-currentFillLevel];
+            liquidImages[maxCapacity - currentFillLevel] = null;
             currentFillLevel--;
-            Image image = liquidImages[currentFillLevel];
-            liquidImages[currentFillLevel] = null; // Clear the reference
             return image;
+        }
+        return null;
+    }
+
+    public Image PeekTopImage()
+    {
+        if (currentFillLevel > 0)
+        {
+            return liquidImages[maxCapacity - currentFillLevel];
         }
         return null;
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,114 +1,386 @@
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.EventSystems;
+//using System.Collections.Generic;
+
+//public class GameManager : MonoBehaviour
+//{
+//    public Bottle[] bottles;
+//    private Bottle selectedBottle = null;
+//    Bottle bottle = new Bottle();
+//    private void Start()
+//    {
+//        foreach (Bottle bottle in bottles)
+//        {
+//            AddEventTrigger(bottle.gameObject);
+//        }
+//    }
+
+//    private void AddEventTrigger(GameObject bottleGameObject)
+//    {
+//        EventTrigger trigger = bottleGameObject.GetComponent<EventTrigger>();
+//        if (trigger == null)
+//        {
+//            trigger = bottleGameObject.AddComponent<EventTrigger>();
+//        }
+
+//        EventTrigger.Entry entry = new EventTrigger.Entry
+//        {
+//            eventID = EventTriggerType.PointerClick
+//        };
+//        entry.callback.AddListener((data) => OnBottleClick(bottleGameObject));
+//        trigger.triggers.Add(entry);
+//    }
+
+//    public void OnBottleClick(GameObject clickedBottleGameObject)
+//    {
+
+
+//        Bottle clickedBottle = clickedBottleGameObject.GetComponent<Bottle>();
+//        if (clickedBottle == null) return;
+
+//        if (selectedBottle == null)
+//        {
+//            selectedBottle = clickedBottle;
+//            Debug.Log("Selected Bottle: " + selectedBottle.name);
+//            HighlightBottle(selectedBottle, true);
+//        }
+//        else
+//        {
+//            HighlightBottle(selectedBottle, false);
+//            if (selectedBottle != clickedBottle && clickedBottle.currentFillLevel < bottle.maxCapacity)
+//            {
+//                Debug.Log("Moving Liquid from " + selectedBottle.name + " to " + clickedBottle.name);
+//                MoveLiquid(selectedBottle, clickedBottle);
+//            }
+//            else
+//            {
+//                Debug.Log("Cannot move liquid to the bottle: " + clickedBottle.name);
+//            }
+
+//            HighlightBottle(selectedBottle, false);
+//            selectedBottle = null;
+//        }
+
+//    }
+
+//    public void MoveLiquid(Bottle fromBottle, Bottle toBottle)
+//    {
+//        Debug.Log("Inside Move Liquid: Moving from " + fromBottle.name + " to " + toBottle.name);
+
+//        if (fromBottle == null || toBottle == null || fromBottle == toBottle) return;
+
+//        List<Image> imagesToMove = new List<Image>();
+//        bool canMove = false;
+
+//        // Gather consecutive images with the same color
+//        while (fromBottle.currentFillLevel > 0)
+//        {
+//            Image currentImage = fromBottle.liquidImages[fromBottle.currentFillLevel - 1];
+//            //Image CurrentImage = fromBottle.liquidImages[bottle.maxCapacty-fromBottle.currentFillLevel-1];
+
+//            if (imagesToMove.Count == 0 || imagesToMove[0].sprite == currentImage.sprite)
+//            {
+//                imagesToMove.Add(currentImage);
+//                fromBottle.RemoveImage(); // Remove image after adding to the list
+//            }
+//            else
+//            {
+//                break; // Stop if color changes
+//            }
+//        }
+
+//        if (toBottle.currentFillLevel == 0)
+//        {
+//            // The destination bottle is empty, so allow the move
+//            canMove = true;
+//        }
+//        else
+//        {
+//            // Check if the last image in the destination bottle matches the color of the first image to move
+//            Image lastImageInToBottle = toBottle.liquidImages[toBottle.currentFillLevel - 1];
+
+//            if (lastImageInToBottle != null && lastImageInToBottle.sprite == imagesToMove[0].sprite)
+//            {
+//                // The sprite of the last image matches the sprite of the images to be moved
+//                canMove = true;
+//            }
+//        }
+
+//        if (canMove)
+//        {
+//            // Check if there's enough space in the destination bottle
+//            if (toBottle.currentFillLevel + imagesToMove.Count <= toBottle.maxCapacity)
+//            {
+//                foreach (Image imageToMove in imagesToMove)
+//                {
+//                    imageToMove.transform.SetParent(toBottle.transform);
+//                    imageToMove.transform.localPosition = new Vector3(0, 0, 0);
+//                    imageToMove.transform.localScale = new Vector3(1, 1, 1);
+
+//                    // Add the image to the target bottle
+//                    toBottle.AddImage(imageToMove);
+//                }
+
+//                Debug.Log("Moved images to " + toBottle.name);
+//            }
+//            else
+//            {
+//                // If not enough space, return images back to the original bottle
+//                foreach (Image imageToMove in imagesToMove)
+//                {
+//                    fromBottle.AddImage(imageToMove);
+//                }
+//                Debug.Log("Cannot move images to " + toBottle.name + " because there is not enough space.");
+//            }
+//        }
+//        else
+//        {
+//            // If the move is not allowed, return the images back to the original bottle
+//            foreach (Image imageToMove in imagesToMove)
+//            {
+//                fromBottle.AddImage(imageToMove);
+//            }
+//            Debug.Log("Cannot move images to " + toBottle.name + " because the colors do not match or the bottle is full.");
+//        }
+//    }
+
+
+
+//    private void HighlightBottle(Bottle bottle, bool highlight)
+//    {
+//        if (highlight)
+//        {
+//            bottle.GetComponent<Image>().color = Color.red;
+//        }
+//        else
+//        {
+//            bottle.GetComponent<Image>().color = Color.white;
+//        }
+
+//    }
+//}
+
+
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-public Bottle[] bottles;
-private Bottle selectedBottle = null;
-Bottle bottle = new Bottle();
-private void Start()
-{
-    foreach (Bottle bottle in bottles)
+    public Bottle[] bottles;
+    private Bottle selectedBottle = null;
+
+    private void Start()
     {
-        AddEventTrigger(bottle.gameObject);
-    }
-}
-
-private void AddEventTrigger(GameObject bottleGameObject)
-{
-    EventTrigger trigger = bottleGameObject.GetComponent<EventTrigger>();
-    if (trigger == null)
-    {
-        trigger = bottleGameObject.AddComponent<EventTrigger>();
-    }
-
-    EventTrigger.Entry entry = new EventTrigger.Entry
-    {
-        eventID = EventTriggerType.PointerClick
-    };
-    entry.callback.AddListener((data) => OnBottleClick(bottleGameObject));
-    trigger.triggers.Add(entry);
-}
-
-public void OnBottleClick(GameObject clickedBottleGameObject)
-{
-
-
-    Bottle clickedBottle = clickedBottleGameObject.GetComponent<Bottle>();
-    if (clickedBottle == null) return;
-
-    if (selectedBottle == null)
-    {
-        selectedBottle = clickedBottle;
-        Debug.Log("Selected Bottle: " + selectedBottle.name);
-        HighlightBottle(selectedBottle, true);
-    }
-    else
-    {
-        HighlightBottle(selectedBottle, false);
-        if (selectedBottle != clickedBottle && clickedBottle.currentFillLevel < bottle.maxCapacity)
+        foreach (Bottle bottle in bottles)
         {
-            Debug.Log("Moving Liquid from " + selectedBottle.name + " to " + clickedBottle.name);
-            MoveLiquid(selectedBottle, clickedBottle);
+            AddEventTrigger(bottle.gameObject);
+        }
+    }
+
+    private void AddEventTrigger(GameObject bottleGameObject)
+    {
+        EventTrigger trigger = bottleGameObject.GetComponent<EventTrigger>();
+        if (trigger == null)
+        {
+            trigger = bottleGameObject.AddComponent<EventTrigger>();
+        }
+
+        EventTrigger.Entry entry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerClick
+        };
+        entry.callback.AddListener((data) => OnBottleClick(bottleGameObject));
+        trigger.triggers.Add(entry);
+    }
+
+    public void OnBottleClick(GameObject clickedBottleGameObject)
+    {
+        Bottle clickedBottle = clickedBottleGameObject.GetComponent<Bottle>();
+        if (clickedBottle == null) return;
+
+        if (selectedBottle == null)
+        {
+            selectedBottle = clickedBottle;
+            Debug.Log("Selected Bottle: " + selectedBottle.name);
+            HighlightBottle(selectedBottle, true);
         }
         else
         {
-            Debug.Log("Cannot move liquid to the bottle: " + clickedBottle.name);
-        }
+            HighlightBottle(selectedBottle, false);
+            if (selectedBottle != clickedBottle && clickedBottle.currentFillLevel < clickedBottle.maxCapacity)
+            {
+                Debug.Log("Moving Liquid from " + selectedBottle.name + " to " + clickedBottle.name);
+                MoveLiquid(selectedBottle, clickedBottle);
+            }
+            else
+            {
+                Debug.Log("Cannot move liquid to the bottle: " + clickedBottle.name);
+            }
 
-        HighlightBottle(selectedBottle, false);
-        selectedBottle = null;
+            HighlightBottle(selectedBottle, false);
+            selectedBottle = null;
+        }
     }
 
-}
+    //public void MoveLiquid(Bottle fromBottle, Bottle toBottle)
+    //{
+    //    Debug.Log("Inside Move Liquid: Moving from " + fromBottle.name + " to " + toBottle.name);
 
-  
+    //    if (fromBottle == null || toBottle == null || fromBottle == toBottle) return;
+
+    //    List<Image> imagesToMove = new List<Image>();
+    //    bool canMove = false;
+
+    //    // Gather consecutive images with the same color from the top of the fromBottle
+    //    while (fromBottle.currentFillLevel > 0)
+    //    {
+    //        Image currentImage = fromBottle.RemoveImage();
+
+    //        if (imagesToMove.Count == 0 || imagesToMove[0].sprite == currentImage.sprite)
+    //        {
+    //            imagesToMove.Add(currentImage);
+    //        }
+    //        else
+    //        {
+    //            fromBottle.AddImage(currentImage); // Put back the image if the colors don't match
+    //            break; // Stop if color changes
+    //        }
+    //    }
+
+    //    if (toBottle.currentFillLevel == 0)
+    //    {
+    //        // The destination bottle is empty, so allow the move
+    //        canMove = true;
+    //    }
+    //    else
+    //    {
+    //        // Check if the top image in the destination bottle matches the color of the first image to move
+    //        Image topImageInToBottle = toBottle.PeekTopImage();
+    //        if (topImageInToBottle != null && topImageInToBottle.sprite == imagesToMove[0].sprite)
+    //        {
+    //            canMove = true;
+    //        }
+    //    }
+
+    //    if (canMove)
+    //    {
+    //        // Check if there's enough space in the destination bottle
+    //        if (toBottle.currentFillLevel + imagesToMove.Count <= toBottle.maxCapacity)
+    //        {
+    //            foreach (Image imageToMove in imagesToMove)
+    //            {
+    //                toBottle.AddImage(imageToMove);
+    //            }
+    //            Debug.Log("Moved images to " + toBottle.name);
+    //        }
+    //        else
+    //        {
+    //            // If not enough space, return images back to the original bottle
+    //            foreach (Image imageToMove in imagesToMove)
+    //            {
+    //                fromBottle.AddImage(imageToMove);
+    //            }
+    //            Debug.Log("Cannot move images to " + toBottle.name + " because there is not enough space.");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        // If the move is not allowed, return the images back to the original bottle
+    //        foreach (Image imageToMove in imagesToMove)
+    //        {
+    //            fromBottle.AddImage(imageToMove);
+    //        }
+    //        Debug.Log("Cannot move images to " + toBottle.name + " because the colors do not match or the bottle is full.");
+    //    }
+    //}
     public void MoveLiquid(Bottle fromBottle, Bottle toBottle)
     {
         Debug.Log("Inside Move Liquid: Moving from " + fromBottle.name + " to " + toBottle.name);
 
         if (fromBottle == null || toBottle == null || fromBottle == toBottle) return;
 
-        // Remove the image from the 'fromBottle'
-        Image imageToMove = fromBottle.RemoveImage();
+        List<Image> imagesToMove = new List<Image>();
+        bool canMove = false;
 
-        if (imageToMove != null)
+        // Gather consecutive images with the same color from the top of the fromBottle
+        while (fromBottle.currentFillLevel > 0)
         {
-            // Set the image's parent to the target bottle
-            imageToMove.transform.SetParent(toBottle.transform);
-            imageToMove.transform.localPosition = new Vector3(0, 0, 0);
-            imageToMove.transform.localScale = new Vector3(1, 1, 1);
+            Image currentImage = fromBottle.RemoveImage();
 
-            // Add the image to the target bottle
-            bool success = toBottle.AddImage(imageToMove);
-
-            if (success)
+            if (imagesToMove.Count == 0 || imagesToMove[0].sprite == currentImage.sprite)
             {
-                Debug.Log("Moved image to " + toBottle.name);
+                imagesToMove.Add(currentImage);
             }
             else
             {
-                Debug.Log("Cannot add image to " + toBottle.name);
+                fromBottle.AddImage(currentImage); // Put back the image if the colors don't match
+                break; // Stop if color changes
+            }
+        }
+
+        if (toBottle.currentFillLevel == 0)
+        {
+            // The destination bottle is empty, so allow the move
+            canMove = true;
+        }
+        else
+        {
+            // Check if the top image in the destination bottle matches the color of the first image to move
+            Image topImageInToBottle = toBottle.PeekTopImage();
+            if (topImageInToBottle != null && topImageInToBottle.sprite == imagesToMove[0].sprite)
+            {
+                canMove = true;
+            }
+        }
+
+        if (canMove)
+        {
+            // Check if there's enough space in the destination bottle
+            if (toBottle.currentFillLevel + imagesToMove.Count <= toBottle.maxCapacity)
+            {
+                for (int i = imagesToMove.Count - 1; i >= 0; i--)
+                {
+                    Image imageToMove = imagesToMove[i];
+
+                    // Update the image's parent and reset its local position
+                    imageToMove.transform.SetParent(toBottle.transform, false);
+                    imageToMove.transform.localPosition = Vector3.zero;
+                    imageToMove.transform.localScale = Vector3.one;
+
+                    // Add the image to the target bottle in the correct order
+                    toBottle.AddImage(imageToMove);
+                }
+                Debug.Log("Moved images to " + toBottle.name);
+            }
+            else
+            {
+                // If not enough space, return images back to the original bottle
+                foreach (Image imageToMove in imagesToMove)
+                {
+                    fromBottle.AddImage(imageToMove);
+                }
+                Debug.Log("Cannot move images to " + toBottle.name + " because there is not enough space.");
             }
         }
         else
         {
-            Debug.Log("No image to move from " + fromBottle.name);
+            // If the move is not allowed, return the images back to the original bottle
+            foreach (Image imageToMove in imagesToMove)
+            {
+                fromBottle.AddImage(imageToMove);
+            }
+            Debug.Log("Cannot move images to " + toBottle.name + " because the colors do not match or the bottle is full.");
         }
     }
 
 
-    private void HighlightBottle(Bottle bottle, bool highlight)
-{
-    if (highlight)
-    {
-        bottle.GetComponent<Image>().color = Color.red;
-    }
-    else
-    {
-        bottle.GetComponent <Image>().color = Color.white;
-    }
 
-}
+
+    private void HighlightBottle(Bottle bottle, bool highlight)
+    {
+        bottle.GetComponent<Image>().color = highlight ? Color.red : Color.white;
+    }
 }
